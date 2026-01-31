f7e1b81906eb24338e63656d9d51b0ad6b1ef998
uifor <uifor@qq.com>
2026-01-31 17:25:43 +0800

    Update diy.sh to add new packages and paths
    Added new package clones and updated package paths.

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-
 # Adjust source code
 patch -p1 -f < $(dirname "$0")/luci.patch
 
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall --depth=1 clone/passwall
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki --depth=1 clone/nikki
+git clone https://github.com/sirpdboy/luci-app-lucky --depth=1 clone/lucky
+git clone https://github.com/sbwml/luci-app-openlist2 --depth=1 clone/openlist
 
 # Update packages
-rm -rf feeds/luci/applications/luci-app-passwall
-cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+cp -rf clone/amlogic/luci-app-amlogic clone/nikki/luci-app-nikki feeds/luci/applications/
+cp -rf clone/lucky feeds/luci/applications/luci-app-lucky
+cp -rf clone/openlist feeds/luci/applications/luci-app-openlist
 sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
 
 # Clean packages
